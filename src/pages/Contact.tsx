4fa7dd22279add1e3dc95a4407ecdf583d0a065f
00x20d <0x20d@proton.me>
2026-05-06 15:18:07 +0200

    updated content type in server req

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -22,6 +22,9 @@ const ContactPage: React.FC = () => {
     try {
       const response = await fetch("/api/contact", {
         method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
         body: JSON.stringify(data),
       });
 
